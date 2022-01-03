c1849e6c0b92a0738871bdd0d6d91bebe6c2f401
Hoppy0616 <hodahoda.20430@gmail.com>
2022-01-03 23:49:01 +0900

    gacchaコマンドのメソッドの記述

--- src/commands/perceptions.ts
+++ src/commands/perceptions.ts
@@ -1,6 +1,7 @@
-import { app } from '../initializers/bolt'
-export default function() {
-  app.command(`/gaccha`, async ({ ack, body, client, logger }) => {
+import { app } from "../initializers/bolt";
+export default function () {
+  app.command(`/gaccha`, async ({ ack, command, say }) => {
     await ack();
-    
-  )}
+    say(`${command.text}`);
+  });
+}
